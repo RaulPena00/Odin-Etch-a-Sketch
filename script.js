766d237aac76e55ee750b282144351c26ed66a5e
Raul Pena Pereira <43591166+RaulPena00@users.noreply.github.com>
2022-09-07 12:16:44 -0300

    Corrected default Grid and added new grid btn
    New grid btn still not functional.

--- script.js
+++ script.js
@@ -1,24 +1,46 @@
 
-
-function makeGrid(x) {
-    const container = document.getElementById("container");
+function newGrid(size){
     
-    for (let rows = 0; rows < x; rows++){
+    for (i = 0; i < size; i++){
         const row = document.createElement('div');
-        for(let columns = 1; columns < x; columns++){
+        for (j = 0; j < size; j++){
             const cell = document.createElement('div');
             cell.className = 'squares';
-            row.appendChild(cell); 
-            cell.addEventListener('mouseover', () => hover(cell))           
-        };
-        container.appendChild(row)
-    };
-};
+            row.appendChild(cell);
+            cell.addEventListener('mouseover', () => hover(cell));
+            }
+            container.appendChild(row);
+        }
+} 
 
+function resetButton(){
+    const size = prompt('Insert the size of the grid');
+    if(size > 100){
+        return alert("Invalid value, please insert a value lesser than 100");
+    }
+    else{
 
+        const grid = document.querySelector('.squares');
+                
+
+        for (i = 0; i < size; i++){
+            const row = document.createElement('div');
+            for (j = 0; j < size; j++){
+                const cell = document.createElement('div');
+                cell.className = 'squares';
+                row.appendChild(cell);
+                cell.addEventListener('mouseover', () => hover(cell));
+                }
+        container.appendChild(row);
+        }
+    }
+}
+
+const btnod = document.getElementById('btn');
+btnod.addEventListener('click', () => resetButton());
 
 function hover(element){
     element.style.backgroundColor = "black";
 }
 
-makeGrid(20)
+newGrid(16)
